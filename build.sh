7bd641e4e5e0972402d4061bdf348885411c314c
tam-pattern <tam@alphahtrading.com>
2023-11-04 17:06:52 +0800

    Remove folder overwrite_crypto_cpp

--- build.sh
+++ build.sh
@@ -1,18 +1,6 @@
 # Building from zero everytime.
 rm -rf build && mkdir build
 
-# Overwrite CMakeLists.txt files to remove test
-rm -rf ./libs/crypto-cpp/CMakeLists.txt
-cp ./overwrite_crypto_cpp/CMakeLists.txt ./libs/crypto-cpp/CMakeLists.txt
-# starkware
-cp ./overwrite_crypto_cpp/CMakeLists_starkware.txt ./libs/crypto-cpp/src/starkware/CMakeLists.txt
-# algebra
-cp ./overwrite_crypto_cpp/CMakeLists_algebra.txt ./libs/crypto-cpp/src/starkware/algebra/CMakeLists.txt
-# crypto
-cp ./overwrite_crypto_cpp/CMakeLists_crypto.txt ./libs/crypto-cpp/src/starkware/crypto/CMakeLists.txt
-# starkex
-cp ./overwrite_crypto_cpp/CMakeLists_starkex.txt ./libs/crypto-cpp/src/starkware/starkex/CMakeLists.txt
-
 cd ./libs/crypto-cpp && rm -rf build
 mkdir build && cd build
 
