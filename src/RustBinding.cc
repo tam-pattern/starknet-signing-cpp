3daea0b5fbe384a7f4e1a26076a7a79c235a1ac9
tam-pattern <tam@alphahtrading.com>
2023-11-13 16:36:33 +0800

    Add extern "C" method process_string()

--- src/RustBinding.cc
+++ src/RustBinding.cc
@@ -4,4 +4,11 @@ extern "C" {
     size_t test_get_jwt_size(size_t source_length) {
         return 1200;
     }
+
+    const char* process_string(const char* input) {
+        std::string input_str(input);
+
+        // Return this string
+        return input_str.c_str();
+    }
 }
--- src/RustBinding.h
+++ src/RustBinding.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <stddef.h>
+#include <string>
 
 extern "C" {
     size_t test_get_jwt_size(size_t source_length);
+    const char* process_string(const char* input);
 }
