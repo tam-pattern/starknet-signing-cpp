5aa2c20cb5d63092e91f3c5026edf846432aa2bb
tam-pattern <tam@alphahtrading.com>
2023-11-14 12:10:59 +0800

    Implement method get_headers_list() for class RustSigner

--- src/RustBinding.cc
+++ src/RustBinding.cc
@@ -1,5 +1,8 @@
 #include "RustBinding.h"
 
+using namespace starkware;
+
+// Utils functions
 char* my_strdup(const char* str) {
     if (str == nullptr) {
         return nullptr;
@@ -9,6 +12,35 @@ char* my_strdup(const char* str) {
     char* new_str = new char[len];
     std::strcpy(new_str, str);
     return new_str;
+}
+
+BigInt<4> hex_string_to_big_int(const std::string &hex_str) {
+  BigInt<4> result = BigInt<4>::Zero();
+
+  // Start with 2nd letter to skip '0x'
+  size_t hex_str_len = hex_str.size();
+  size_t i = (hex_str[0] == '0' && hex_str[1] == 'x') ? 2 : 0;
+
+  // [module] should be big enough, in this case [modulo] = 16 ^ 65 - 1
+  BigInt<4> modulo =
+      0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff_Z;
+
+  // Loop through each letter in [hex_str] and at it to [result].
+  while (i < hex_str_len) {
+    uint64_t hexDigit = bigint::details::HexCharToUint64(hex_str[i]);
+    result = BigInt<4>::MulMod(result, (BigInt<4>)16, modulo);
+    result = result + (BigInt<4>)hexDigit;
+
+    i++;
+  }
+
+  return result;
+}
+
+BigInt<4> double_to_big_int(double val) {
+  uint64_t integer_val = val * 100000000;
+
+  return Uint256(integer_val);
 }
 
 StringVector StringVector::from_cpp_vector_string(const std::vector<std::string>& cpp_vector_string)
@@ -26,12 +58,18 @@ StringVector StringVector::from_cpp_vector_string(const std::vector<std::string>
 }
 
 RustSigner::RustSigner(const std::string& starknet_address, const std::string& starknet_private_address, const std::string& chain_id)
-    : starknet_address_(starknet_address), starknet_private_address_(starknet_private_address), chain_id_(chain_id)
+    :   starknet_address_(hex_string_to_big_int(starknet_address)),
+        starknet_private_key_(hex_string_to_big_int(starknet_private_address)),
+        chain_id_(chain_id),
+        key_pair_(starknet_private_key_),
+        starknet_address_prime_field_element_(
+            PrimeFieldElement::FromBigInt(starknet_address_))
 {}
 
-StringVector RustSigner::get_header_list()
+StringVector RustSigner::get_headers_list()
 {
-    return StringVector::from_cpp_vector_string({starknet_address_, starknet_private_address_, chain_id_});
+    std::vector<std::string> headers_list = Account(starknet_address_prime_field_element_, chain_id_, key_pair_).getHeadersList("");
+    return StringVector::from_cpp_vector_string(headers_list);
 }
 
 extern "C" {
@@ -48,7 +86,7 @@ extern "C" {
         return "Hello, world!";
     }
 
-    StringVector get_header_list(const char* starknet_address, const char* starknet_private_address, const char* chain_id) {
-        return RustSigner(starknet_address, starknet_private_address, chain_id).get_header_list();
+    StringVector get_headers_list(const char* starknet_address, const char* starknet_private_address, const char* chain_id) {
+        return RustSigner(starknet_address, starknet_private_address, chain_id).get_headers_list();
     }
 }
--- src/RustBinding.h
+++ src/RustBinding.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <stddef.h>
-#include <assert.h>
-#include <stdlib.h>
 #include <cstring>
 #include <string>
 #include <vector>
+
+#include "Account.hpp"
+#include "Order.hpp"
+#include "StarkCurveSigner.hpp"
+#include "StarknetDomain.hpp"
+#include <starkware/algebra/big_int.h>
+#include <starkware/algebra/prime_field_element.h>
+
+using namespace starkware;
+using namespace signer;
 
 // Struct to represent vector of strings
 struct StringVector {
@@ -18,13 +26,15 @@ struct StringVector {
 class RustSigner
 {
 private:
-    std::string starknet_address_;
-    std::string starknet_private_address_;
+    const BigInt<4> starknet_address_;
+    const BigInt<4> starknet_private_key_;
     std::string chain_id_;
+    const KeyPair key_pair_;
+    const PrimeFieldElement starknet_address_prime_field_element_;
 
 public:
     RustSigner(const std::string& starknet_address, const std::string& starknet_private_address, const std::string& chain_id);
-    StringVector get_header_list();
+    StringVector get_headers_list();
 };
 
 extern "C" {
